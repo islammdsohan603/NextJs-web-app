bd7f2c9ec105ebcedd3f87fb73219ca0d0956bd2
islammdsohan603 <islammdsohan603@gmail.com>
2026-04-26 22:05:03 +0600

    user name show in navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,29 +1,53 @@
+'use client';
+
 import React from 'react';
 import NavbLink from './NavbLink';
-
-const navbar = [
-  { id: 1, name: 'Home', link: '/' },
-  { id: 3, name: 'SignUp', link: '/signUp' },
-  { id: 2, name: 'LogIn', link: '/login' },
-];
+import { authClient } from '@/lib/auth-client';
 
 const Navbar = () => {
+  const { data: session, isPending } = authClient.useSession();
+
+  const handleLogout = async () => {
+    await authClient.signOut();
+  };
+
   return (
-    <div>
-      <header className="bg-linear-to-bl from-[#03045e] to-[#10002b] py-6">
-        <nav className="flex items-center justify-between w-10/12 mx-auto">
-          <h1 className="text-lg font-bold text-white">Auth</h1>
-          <ul className="flex items-center gap-4">
-            {navbar.map(navItem => (
-              <NavbLink href={navItem.link} key={navItem.id}>
-                {' '}
-                {navItem.name}{' '}
-              </NavbLink>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header className="bg-linear-to-bl from-[#03045e] to-[#10002b] py-6">
+      <nav className="flex items-center justify-between w-10/12 mx-auto">
+        <h1 className="text-lg font-bold text-white">Auth</h1>
+
+        {/* Navbar Links */}
+        <ul className="flex items-center gap-6">
+          <NavbLink href="/">Home</NavbLink>
+
+          {/* User না থাকলে Login + Signup দেখাবে */}
+          {!session?.user && (
+            <>
+              <NavbLink href="/signUp">SignUp</NavbLink>
+              <NavbLink href="/login">LogIn</NavbLink>
+            </>
+          )}
+
+          {/* User Section */}
+          <div className="text-white">
+            {isPending ? (
+              <p>Loading...</p>
+            ) : session?.user ? (
+              <div className="flex items-center gap-4">
+                <h1 className="font-semibold">{session.user.name}</h1>
+
+                <button
+                  onClick={handleLogout}
+                  className="bg-red-500 px-4 py-2 rounded-xl hover:bg-red-600 transition"
+                >
+                  Logout
+                </button>
+              </div>
+            ) : null}
+          </div>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
