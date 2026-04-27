2a3a62c052788c3dd32268506ffffd41371d853e
islammdsohan603 <islammdsohan603@gmail.com>
2026-04-27 14:10:01 +0600

    error handle

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -21,13 +21,12 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
-  },
-
-  socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     },
   },
+
+
 
 });
